5a279c6fdca2be4034f787e47176dd8c3fed3240
asraghuvanshi <asraghuvanshi52gmail.com>
2023-02-21 23:56:48 +0530

    typealias to replace existing name

---
+++ SwiftCore/typeAlias.swift
@@ -0,0 +1,9 @@
+let name:String = "Krishna Programmer"
+
+typealias employee_name = String
+var name:employee_name = "Krishna Programmer"
+
+print(name)
+var dict = [1:"First" , 2:"Second", 3:"Third", 4:"Fourth" , 5:"Fifth" , 6:"Sixth"]
+dict[7] = "Seventh"
+print(dict , dict[7]!)
