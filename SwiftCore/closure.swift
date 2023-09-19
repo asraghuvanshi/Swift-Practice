b1ca78ca19129a0b23caeee92a5133734fbac82a
A.S Raghuvanshi <110293033+asraghuvanshi@users.noreply.github.com>
2023-09-19 22:47:15 +0530

    Update closure.swift

--- SwiftCore/closure.swift
+++ SwiftCore/closure.swift
@@ -12,3 +12,11 @@ print(obj)
 var arr = [10,20,30,40,50,60]
 let result = { (item)-> Int in return item * 2}
 print(arr.map(result))
+
+
+let isEvenAlt = { (i: Int8) -> Bool in i % 2 == 0 }
+print(isEvenAlt(6))
+print("sorted array", arr.sorted(by: >))
+
+var tuple_data = [(2,"Swift") , (1, "Java"), (3, "Ruby"), (4, "Python"), (5, "Pearl")]
+print(tuple_data.sorted(by: >))
